f025577098c80f928f605cb549fd93e10e1d997c
Dwarika Mandal <dwarika_mandal@persistent.co.in>
2017-06-02 10:59:59 +0530

    Update manifest file to change static route and Call WCS directly

--- src/app/chat.component.ts
+++ src/app/chat.component.ts
@@ -10,14 +10,20 @@ import { Message } from './message.object';
     providers: [ChatService]
 })
 
-export class ChatComponent implements OnInit, AfterViewChecked  {
+export class ChatComponent implements OnInit, AfterViewChecked {
+    inpuData: any;
+    wcs_context: any ={};
+    wcs_response: any ;
 
-@ViewChild('scrollMe') private myScrollContainer: ElementRef;
+    @ViewChild('scrollMe') private myScrollContainer: ElementRef;
     messageList: Array<Message> = new Array<Message>();
-    inputMessage: string;
+    inputMessage: string="";
     outputMessage: string;
 
-    constructor(private chatService: ChatService) { }
+    constructor(private chatService: ChatService) {
+        // let msg = new Message("Hi. Welcome to virtual assistant of your car. What Can I do for you.", false);
+        // this.messageList.push(msg);
+    }
 
     sendQuery(query: string): void {
 
@@ -25,21 +31,23 @@ export class ChatComponent implements OnInit, AfterViewChecked  {
 
     ngOnInit() {
 
-        this.chatService.getResponse("").subscribe(res => {
-            this.outputMessage = res;
-            let msg = new Message(res, false);
-            this.messageList.push(msg);
-        })
+        // this.chatService.getResponse("").subscribe(res => {
+        //     this.outputMessage = res;
+        //     let msg = new Message(res, false);
+        //     this.messageList.push(msg);
+        // })
+
+        this.postDataConversation();
     };
 
-    ngAfterViewChecked() {        
-        this.scrollToBottom();        
-    } 
+    ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
 
-     scrollToBottom(): void {
+    scrollToBottom(): void {
         try {
             this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
-        } catch(err) { }                 
+        } catch (err) { }
     }
 
     sendMessage(): void {
@@ -48,8 +56,63 @@ export class ChatComponent implements OnInit, AfterViewChecked  {
         this.messageList.push(msg);
         this.chatService.getResponse(this.inputMessage).subscribe(res => {
             msg = new Message(res[0].toString(), false);
-            this.messageList.push(msg);           
+            this.messageList.push(msg);
+            this.inputMessage = "";
+        })
+    }
+
+    postDataConversation(): void {
+      
+        this.inpuData = {
+            "input": {
+                "text": this.inputMessage
+            },
+            "context": this.wcs_context
+        }
+
+        let msg = new Message(this.inputMessage, true);
+        if(this.inputMessage !=""){
+            this.messageList.push(msg);
+        }
+
+        this.chatService.postMethod(this.inpuData).subscribe(res => {
+
+            this.wcs_response = res;
+            msg = new Message(res.output.text[0].toString(), false);
+            this.wcs_context = res.context;
+            this.messageList.push(msg);
             this.inputMessage = "";
         })
     }
 }
+
+
+// this.wcs_response = {
+//     "intents": [],
+//     "entities": [],
+//     "input": {},
+//     "output": {
+//         "text": ["Hi. Welcome to virtual assistant of your car. What Can I do for you?"],
+//         "nodes_visited": ["Conversation Start"],
+//         "log_messages": []
+//     },
+//     "context": {
+//         "conversation_id": "da7b0ec9-ffe6-4f03-962a-0198a96b5676",
+//         "system": {
+//             "dialog_stack": [{ "dialog_node": "root" }],
+//             "dialog_turn_counter": 1,
+//             "dialog_request_counter": 1,
+//             "_node_output_map": { "Conversation Start": [0] },
+//             "branch_exited": true,
+//             "branch_exited_reason": "completed"
+//         },
+//         "AConoff": "off",
+//         "lightonoff": "off",
+//         "musiconoff": "off",
+//         "appl_action": "",
+//         "heateronoff": "off",
+//         "volumeonoff": "off",
+//         "wipersonoff": "off",
+//         "default_counter": 0
+//     }
+// }
--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,25 +1,67 @@
 
 import { Injectable } from '@angular/core';
-import { Http, Jsonp, URLSearchParams  } from '@angular/http';
+import { Http, Jsonp, URLSearchParams, Headers, RequestOptions, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 
 export class ChatService {
 
+    serviceUrl: string ="https://gateway.watsonplatform.net/conversation/api/v1/workspaces/60f35549-55f2-4ddb-8fc5-efb760f261e9/message?version=2017-05-31";
+
     constructor(private http: Http, private jsonp: Jsonp) {
     }
 
-    getResponse(text:string): Observable<string> {
-           
-        return this.http.get("https://iot-training-dwarika.mybluemix.net/chats",  {params: { inputText: text }}).map(response => response.json())
+    getResponse(text: string): Observable<string> {
+
+        return this.http.get("https://iot-training-dwarika.mybluemix.net/chats", { params: { inputText: text } }).map(response => response.json())
     }
 
     postQuery(data: string): Observable<string> {
         let options = new URLSearchParams();
-        options.append('input', "turn on");        
+        options.append('input', "turn on");
         return this.http.post("https://iot-training-dwarika.mybluemix.net/chats", "").map(response => response.json());
     }
+
+    /******** Experimental Code  Starts ********/
+
+    getMethod(): Observable<any> {
+        return this.http.get(this.serviceUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    postMethod(postData: any): Observable<any> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        headers.set('Authorization', 'Basic ' + btoa('df1d73ce-9d69-4437-82fe-83c7bcdc1aa2:R7lWEA6Y3t5z'));
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(this.serviceUrl, postData , options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body || {};
+    }
+
+    private handleError(error: Response | any) {
+        // In a real world app, you might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+
+    /******** Experimental Code  Ends ********/
+
 }
